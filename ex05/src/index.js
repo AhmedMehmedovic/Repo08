44e7d797b8fd443701bc14007be113e4a156a707
AhmedMehmedovic <ahmedm.bjelave@gmail.com>
2021-09-20 23:16:59 +0200

    without var in function

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,10 +1,11 @@
 // Only change code below this line
 var dress = "little dress";
 function myDress() {
-    var dress = "ball gown";
+     dress = "ball gown";
 // Only change code above this line
 return dress;
 }
+
 // Only change code above this line
 myDress();
 // Only change code above this line
